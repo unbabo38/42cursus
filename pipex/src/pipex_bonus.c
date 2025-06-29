f47df9c61f0619fad36f5dc7d47fc78f3f5d7f23
beraskes@gmail.com <beraskes@gmail.com>
2025-06-29 21:40:51 +0900

    pipex bonus

--- pipex/src/pipex_bonus.c
+++ pipex/src/pipex_bonus.c
@@ -66,119 +66,91 @@ char *get_cmd_path(char *arg_cmd, char **envp)
 	return (NULL);
 }
 
+void	exec(char *arg, int argc, char **envp)
+{
+
+	char **cmd_args = ft_split(arg, ' ');
+	char *cmd_path;
+	if (ft_strchr(arg, '/'))
+	{
+		if (access(arg, F_OK) != 0)
+			perror("File does not exist");
+		if (access(arg, X_OK) != 0)
+			perror("Permission denied");
+		cmd_path = arg;
+	}
+	else
+		cmd_path = get_cmd_path(cmd_args[0], envp);
+
+	if (!cmd_path)
+	{
+		fprintf(stderr, "command '%s' not found\n", cmd_args[0]);
+		exit(EXIT_FAILURE);
+	}
+	execve(cmd_path, cmd_args, envp);
+	perror("execve failed");
+	exit(EXIT_FAILURE);
+}
+
+
+
 
 int main(int argc, char **argv, char **envp)
 {
-    int     pipe_odd[2];
-	int     pipe_even[2];
-    int     infile;
+    int     fd[2];
+	int     infile;
     int     outfile;
-
     pid_t   pid2;
-    // 1. 引数チェック（argc != 5 のときエラー出力して終了）
-	if (argc < 5)
+	if (argc  5)
 	{
 		perror("invalid num of command");
-		//exit(EXIT_FAILURE);
 		return (0);
 	}
-    // 2. infile を O_RDONLY で open
 	infile = open(argv[1], O_RDONLY);
-    //    - 失敗したら perror + exit
 	if (infile < 0)
 	{
 		perror("infile");
 		return (1);
     }
-
-	// 3. outfile を O_WRONLY | O_CREAT | O_TRUNC で open
 	outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    //    - 失敗したら perror + close(infile) + exit
 	if (outfile < 0)
 	{
 		perror("outfile");
 		return (1);
 	}
 	int	i = 2;
-    while (i < argc - 1)
+	dup2(infile, STDIN_FILENO);
+
+    while (i < argc - 2)
 	{
 		pid_t   pid;
-		if (i % 2 == 1)
+		if (pipe(fd) < 0)
 		{
-			close(pipe_odd[0]);
-    		close(pipe_odd[1]);
-			if (pipe(pipe_odd) < 0)
-			{
-				perror("pipe1");
-				return (1);
-			}
+			perror("pipe error");
 		}
-		else
-		{
-		    close(pipe_even[0]);
-    		close(pipe_even[1]);
-			if (pipe(pipe_even) < 0)
-			{
-				perror("pipe2");
-				return (1);
-			}
-		}
-    // 5. fork() して pid1 を作成（cmd1用の子プロセス）
-    //    - pid1 == 0 のとき：cmd1 処理を実行（さっきの枠）
 		pid = fork();
 		if (pid == 0)
 		{
-			if (i == 2)
-				dup2(infile, STDIN_FILENO);
-			else if (i % 2 == 0)
-				dup2(pipe_odd[0], STDIN_FILENO);
-			else
-				dup2(pipe_even[0], STDIN_FILENO);
+			close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+			exec(argv[i], argc, envp);
+		}
+		else
+		{
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
 
-			if (i == argc - 2)
-				dup2(outfile, STDOUT_FILENO);
-			else if (i % 2 == 0)
-				dup2(pipe_even[1], STDOUT_FILENO);
-			else
-				dup2(pipe_odd[1], STDOUT_FILENO);
-
-			close_all_unused_fds(pipe_odd, pipe_even, i);
-			if (i == argc - 1)
-				exit(EXIT_SUCCESS);
-			char **cmd_args = ft_split(argv[i], ' ');
-			char *cmd_path;
-			if (ft_strchr(argv[i], '/'))
-			{
-				if (access(argv[i], F_OK) != 0)
-					perror(argv[i]);
-				if (access(argv[i], X_OK) != 0)
-					perror("Permission denied");
-
-				cmd_path = argv[i];
-			}
-			else
-			{
-				cmd_path = get_cmd_path(cmd_args[0], envp);
-			}
-
-			if (!cmd_path)
-			{
-				i++;
-				fprintf(stderr, "command '%s' not found\n", cmd_args[0]);
-				exit(EXIT_FAILURE);
-			}
-			execve(cmd_path, cmd_args, envp);
-			perror("execve failed");
-			exit(EXIT_FAILURE);
 		}
 		i++;
 	}
-	close(pipe_odd[0]);
-	close(pipe_odd[1]);
-	close(pipe_even[0]);
-	close(pipe_even[1]);
-
-	for (int j = 0; j < argc - 3; j++)
+	for (int i = 0; i < argc - 2 ; i++)
+	{
 		wait(NULL);
+	}
+	dup2(outfile, STDOUT_FILENO);
+	exec(argv[argc - 2], argc - 2, envp);
+
+	if (i == argc - 2)
+		exit(EXIT_SUCCESS);
 	return (0);
 }
