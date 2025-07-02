82605bfa6937d2decd33f59a2409449e0fc9f7cb
Taiichiro Mura <tmura@c1r4s7.42tokyo.jp>
2025-07-02 22:02:22 +0900

    @school 7/1

--- pipex/src/pipex_bonus.c
+++ pipex/src/pipex_bonus.c
@@ -1,12 +1,4 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "../libft/libft.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
+#include "../include/pipex_bonus.h"
 
 int	get_next_line(char **line)
 {
@@ -37,9 +29,11 @@ int	get_next_line(char **line)
 	return (r);
 }
 
-void free_split(char **contents)
+void	free_split(char **contents)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (contents[i])
 	{
 		free(contents[i]);
@@ -47,14 +41,20 @@ void free_split(char **contents)
 	}
 	free(contents);
 }
-char *resolve_command(char *arg_cmd, char *cmd_env)
+
+char	*resolve_command(char *arg_cmd, char *cmd_env)
 {
-	char **cmd_paths = ft_split(cmd_env, ':');
-	int i = 0;
+	char	**cmd_paths;
+	int		i;
+	char	*cmd;
+	char	*joined_slash;
+
+	cmd_paths = ft_split(cmd_env, ':');
+	i = 0;
 	while (cmd_paths[i])
 	{
-		char *cmd = cmd_paths[i];
-		char *joined_slash = ft_strjoin(cmd, "/");
+		cmd = cmd_paths[i];
+		joined_slash = ft_strjoin(cmd, "/");
 		cmd = ft_strjoin(joined_slash, arg_cmd);
 		free(joined_slash);
 		if (access(cmd, F_OK) == 0 && access(cmd, X_OK) == 0)
@@ -68,37 +68,34 @@ char *resolve_command(char *arg_cmd, char *cmd_env)
 	return (NULL);
 }
 
-char *get_cmd_path(char *arg_cmd, char **envp)
+char	*get_cmd_path(char *arg_cmd, char **envp)
 {
-	int i = 0;
-	char *cmd_env;
+	int		i;
+	char	*cmd_env;
 
+	i = 0;
 	while (envp[i])
 	{
 		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
 		{
 			cmd_env = envp[i] + 5;
-			break;
+			break ;
 		}
 		i++;
 	}
-	return(resolve_command(arg_cmd, cmd_env));
-
+	return (resolve_command(arg_cmd, cmd_env));
 }
 
 int	check_cmd_exist(char *arg)
 {
 	if (access(arg, F_OK) != 0)
 	{
-		//fprintf(stderr, "%s: %s: %s\n", "bash", arg, strerror(errno));
-		perror("bash");
-
+		perror(arg);
 		return (0);
 	}
 	if (access(arg, X_OK) != 0)
 	{
-		//fprintf(stderr, "%s: %s: %s\n", "bash", arg, strerror(errno));
-		perror("bash");
+		perror(arg);
 		return (0);
 	}
 	return (1);
@@ -106,48 +103,55 @@ int	check_cmd_exist(char *arg)
 
 void	exec(char *arg, int argc, char **envp)
 {
-	char **cmd_args = ft_split(arg, ' ');
-	char *cmd_path;
+	char	**cmd_args;
+	char	*cmd_path;
+
+	cmd_args = ft_split(arg, ' ');
 	if (ft_strchr(arg, '/'))
 	{
-		if (!check_cmd_exist)
-			exit(EXIT_FAILURE);
+		if (!check_cmd_exist(arg))
+			exit(127);
 		cmd_path = arg;
 	}
 	else
 		cmd_path = get_cmd_path(cmd_args[0], envp);
 	if (!cmd_path)
 	{
-		fprintf(stderr, "command '%s' not found\n", cmd_args[0]);
-		exit(EXIT_FAILURE);
+		perror(cmd_args[0]);
+		exit(127);
 	}
 	execve(cmd_path, cmd_args, envp);
 	perror("execve failed");
 	exit(EXIT_FAILURE);
 }
 
-void here_document(char *limiter, int argc)
+void	here_document(char *limiter, int argc)
 {
-	int fd[2];
-	char *line;
-	pipe(fd);
-	int process = fork();
+	int		fd[2];
+	char	*line;
+	int		process;
 
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	process = fork();
 	if (process == 0)
 	{
-		while(1)
+		while (1)
 		{
 			write(1, "> ", 2);
 			if (!get_next_line(&line))
 			{
 				write(1, "\n", 1);
+				perror("line");
 				exit(EXIT_SUCCESS);
 			}
 			if (ft_strncmp(limiter, line, ft_strlen(limiter)) == 0)
 				exit(EXIT_SUCCESS);
 			write(fd[1], line, ft_strlen(line));
 		}
-
 	}
 	else
 	{
@@ -156,11 +160,12 @@ void here_document(char *limiter, int argc)
 	}
 }
 
-void do_pipe(int i, char **argv, int argc, char **envp)
+void	do_pipe(int i, char **argv, int argc, char **envp)
 {
-	int     fd[2];
-	pid_t   pid;
-	while(i < argc - 2)
+	int		fd[2];
+	pid_t	pid;
+
+	while (i < argc - 2)
 	{
 		if (pipe(fd) < 0)
 		{
@@ -182,27 +187,27 @@ void do_pipe(int i, char **argv, int argc, char **envp)
 	}
 }
 
-void wait_children(int i, int argc)
+void	wait_children(int i, int argc)
 {
-	while(i < argc - 2)
+	while (i < argc - 2)
 	{
 		wait(NULL);
 		i++;
 	}
 }
 
-void last_exec(char *arg, int argc, char **envp, int outfile)
+void	last_exec(char *arg, int argc, char **envp, int outfile)
 {
-		if (dup2(outfile, STDOUT_FILENO) == -1)
-			fprintf(stderr, "%s: %s: %s\n", "bash", arg, strerror(errno));
-		exec(arg, argc, envp);
-		exit(EXIT_SUCCESS);
+	if (dup2(outfile, STDOUT_FILENO) == -1)
+		perror(arg);
+	exec(arg, argc, envp);
+	exit(EXIT_SUCCESS);
 }
 
-int process_input(char **argv, int argc)
+int	process_input(char **argv, int argc)
 {
-	int infile;
-	int i;
+	int	infile;
+	int	i;
 
 	i = 0;
 	infile = 0;
@@ -217,20 +222,29 @@ int process_input(char **argv, int argc)
 		infile = open(argv[1], O_RDONLY);
 		if (infile < 0)
 		{
-			//fprintf(stderr, "%s: %s: %s\n", "bash", argv[1], strerror(errno));
-			perror("bash");
-
-			return 0;
+			perror(argv[1]);
+			infile = open("/dev/null", O_RDONLY);
+			if (infile < 0)
+			{
+				perror("/dev/null");
+				exit(EXIT_FAILURE);
+			}
 		}
-		dup2(infile, STDIN_FILENO);
+		if (dup2(infile, STDIN_FILENO) < 0)
+		{
+			perror("dup2 infile");
+			close(infile);
+			exit(EXIT_FAILURE);
+		}
+		close(infile);
 	}
 	return (i);
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	int		i;
-    int     outfile;
+	int		outfile;
 
 	i = 0;
 	outfile = 0;
@@ -239,10 +253,17 @@ int main(int argc, char **argv, char **envp)
 		i = process_input(argv, argc);
 		if (!i)
 			return (0);
-		outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (i == 3)
+			outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+		else
+			outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (outfile < 0)
+		{
+			perror(argv[argc - 1]);
+			return (0);	
+		}
 		do_pipe(i, argv, argc, envp);
 		wait_children(i, argc);
-		printf("now");
 		last_exec(argv[argc - 2], argc - 2, envp, outfile);
 	}
 	return (0);
