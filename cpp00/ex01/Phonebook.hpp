55e52229da5c1b579aa064a8a0a4ec76b0ef5a2a
beraskes@gmail.com <beraskes@gmail.com>
2026-03-13 14:32:15 +0900

    cpp00

--- cpp00/ex01/Phonebook.hpp
+++ cpp00/ex01/Phonebook.hpp
@@ -5,18 +5,24 @@
 # include "Contact.hpp"
 
 
-class	Phonebook
+class	PhoneBook
 {
 	private:
 		Contact _contacts[8];
 		int		_index;
+		bool	_isfull;
 	public:
-		Phonebook(void);
-		~Phonebook(void);
+		PhoneBook(void);
+		~PhoneBook(void);
 		void	add(void);
 		void	search(void);
-		void	print(Contact contact);
+		void	print(void);
 		Contact get_contact(int	index);
+
+		std::string	get_str(std::string str);
+		std::string	get_input(std::string type);
+
+
 };
 
 #endif
