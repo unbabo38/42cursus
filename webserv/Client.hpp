ae34254ba8cb69618e6e9a3c06b5fee45181774f
beraskes@gmail.com <beraskes@gmail.com>
2026-06-14 11:31:25 +0900

    response and refactored

--- webserv/Client.hpp
+++ webserv/Client.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include "ConfigServer.hpp"
+#include "Response.hpp"
 
 class Client {
   private:
@@ -20,16 +21,19 @@ class Client {
 	std::string  _resource;
 	std::string  _httpVersion;
 	std::string  _body;
+    std::map<int, std::string>         _error_pages_map;
 
 	std::map<std::string, std::string> _fields;
 	size_t		 _contentLength;
+	Response 	_res;
 
   public:
     Client();
     ~Client();
 	void setRequest(const char* buf, const int &len);
 	const std::string &getRequest() const;
-	const std::string inspectRequest();
+	const std::string &getRequestTarget() const;
+	void inspectRequest();
 	void parseHeader(std::string &header);
     void checkRequestTarget();
     void checkHttpVersion();
@@ -37,6 +41,19 @@ class Client {
 	void parseFields(std::string request, size_t i);
 	void parseCompleted();
 	void checkMethod();
+	const int &getStatusCode() const;
+    const std::map<int, std::string>&   getErrorPagesMap() const;
+    void setErrorPagesMap(const std::map<int, std::string>& error_pages);
+    const size_t &getContentLength() const;
+	const bool &getParseCompleted() const;
+	void setResponseStr(const std::string& str);
+
+    std::string getResponseStr() const;
+	const ConfigServer &getServer() const;
+	void setServer(ConfigServer &server);
+
+
+
 
 };
 
