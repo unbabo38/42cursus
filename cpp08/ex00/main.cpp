0bde91752ae193d44e89150833e3f5892f43938a
beraskes@gmail.com <beraskes@gmail.com>
2026-08-11 18:18:08 +0900

    cpp

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,11 +1,15 @@
 #include "easyfind.hpp"
 
 int main() {
-  std::vector<int> n(10);
-  n[3] = 12;
-  try {
-    easyfind(n, 3);
-  } catch (const std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
+    std::vector<int> n(10, 0);
+    n[3] = 42;
+
+    try {
+        std::vector<int>::iterator res = easyfind(n, 42);
+        std::cout << "found: " << *res << std::endl;
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -10,38 +10,14 @@ private:
 public:
   Span(unsigned int n);
   ~Span();
-  void addNumber(unsigned int n) {
-    if (_nums.size() >= _max_size)
-      throw std::out_of_range("span is full!");
-    _nums.push_back(n);
-  };
-
-  int shortestSpan() {
-    if (_nums.size() <= 1) {
-      throw std::logic_error("not enough element");
-    }
-    std::sort(_nums.begin(), _nums.end());
-    int shortestspan = UINT16_MAX - 1;
-    for (int i = 0; i < _nums.size() - 1; i++) {
-      int nowspan = _nums[i + 1] - _nums[i];
-      if (nowspan < shortestspan)
-        shortestspan = nowspan;
-    }
-    return shortestspan;
-  }
-  int longestSpan() {
-    if (_nums.size() <= 1) {
-      throw std::logic_error("not enough element");
-    }
-    std::sort(_nums.begin(), _nums.end());
-    return _nums.back() - _nums.front();
-  }
-
+  void addNumber(unsigned int n);
+  unsigned int  shortestSpan();
+  unsigned int  longestSpan();
   template <typename InputIterator>
   void addNumbers(InputIterator begin, InputIterator end) {
-    if (_nums.size() + std::distance(begin, end) > _max_size) {
+    if (this->_nums.size() + std::distance(begin, end) > this->_max_size) {
       throw std::out_of_range("span will be full after adding these numbers!");
     }
-    _nums.insert(_nums.end(), begin, end);
+    this->_nums.insert(this->_nums.end(), begin, end);
   }
 };
